0a40534eb50d80ede54f9bfa1bf2b244048bf407
Eva Short <evashort73@gmail.com>
2022-04-14 21:30:37 -0700

    vue hello world

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello World" />
+</template>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
