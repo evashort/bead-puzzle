3f1865811018b628ad17e54fe54ccd56f6f80c55
Eva Short <evashort73@gmail.com>
2022-04-18 17:53:31 -0700

    refactor path control point properties

--- src/components/Edge.vue
+++ src/components/Edge.vue
@@ -8,8 +8,18 @@ const easeOut = cubicBezier(0, 0, 0.58, 1)
 
 export default {
   data() {
-    let d1 = this.getD1(this.x0, this.y0)
-    let d2 = this.getD2(this.x3, this.y3)
+    let d1 = this.getD(
+      this.x2 - this.x1,
+      this.y2 - this.y1,
+      this.x0 - this.x1,
+      this.y0 - this.y1,
+    )
+    let d2 = this.getD(
+      this.x1 - this.x2,
+      this.y1 - this.y2,
+      this.x3 - this.x2,
+      this.y3 - this.y2,
+    )
     return {
       activeAnim: new AnimProp(this.active ? 1 : 0, 150, easeOut),
       dx1Anim: new AnimProp(d1.x, 150, easeOut),
@@ -37,27 +47,27 @@ export default {
     dashOffset() {
       return 0.06 * this.activeAnim.value
     },
-    path() {
-      return `M ${this.x1} ${this.y1} C ${this.x1 + this.dx1Anim.value} ${this.y1 + this.dy1Anim.value}, ${this.x2 + this.dx2Anim.value} ${this.y2 + this.dy2Anim.value}, ${this.x2} ${this.y2}`
-    }
-  },
-  methods: {
-    getD1(x0, y0) {
+    d1() {
       return this.getD(
         this.x2 - this.x1,
         this.y2 - this.y1,
-        x0 - this.x1,
-        y0 - this.y1,
+        this.x0 - this.x1,
+        this.y0 - this.y1,
       )
     },
-    getD2(x3, y3) {
+    d2() {
       return this.getD(
         this.x1 - this.x2,
         this.y1 - this.y2,
-        x3 - this.x2,
-        y3 - this.y2,
+        this.x3 - this.x2,
+        this.y3 - this.y2,
       )
     },
+    path() {
+      return `M ${this.x1} ${this.y1} C ${this.x1 + this.dx1Anim.value} ${this.y1 + this.dy1Anim.value}, ${this.x2 + this.dx2Anim.value} ${this.y2 + this.dy2Anim.value}, ${this.x2} ${this.y2}`
+    },
+  },
+  methods: {
     getD(thisX, thisY, otherX, otherY) {
       let otherLength = Math.sqrt(otherX * otherX + otherY * otherY)
       let thisLength = Math.sqrt(thisX * thisX + thisY * thisY)
@@ -72,25 +82,13 @@ export default {
     active(newActive, oldActive) {
       this.activeAnim.set(newActive ? 1 : 0)
     },
-    x0(newX0, oldX0) {
-      let d1 = this.getD1(newX0, this.y0)
-      this.dx1Anim.set(d1.x)
-      this.dy1Anim.set(d1.y)
+    d1(newD1, oldD1) {
+      this.dx1Anim.set(newD1.x)
+      this.dy1Anim.set(newD1.y)
     },
-    y0(newY0, oldY0) {
-      let d1 = this.getD1(this.x0, newY0)
-      this.dx1Anim.set(d1.x)
-      this.dy1Anim.set(d1.y)
-    },
-    x3(newX3, oldX3) {
-      let d2 = this.getD2(newX3, this.y3)
-      this.dx2Anim.set(d2.x)
-      this.dy2Anim.set(d2.y)
-    },
-    y3(newY3, oldY3) {
-      let d2 = this.getD2(this.x3, newY3)
-      this.dx2Anim.set(d2.x)
-      this.dy2Anim.set(d2.y)
+    d2(newD2, oldD2) {
+      this.dx2Anim.set(newD2.x)
+      this.dy2Anim.set(newD2.y)
     },
   },
 }
