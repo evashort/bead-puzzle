ba70911edbc4597e73825f00e8c7fcde6751bcb5
Eva Short <evashort73@gmail.com>
2022-07-12 21:50:40 -0700

    arrow keys select incoming edge

--- src/components/Bead.vue
+++ src/components/Bead.vue
@@ -1,177 +1,33 @@
-<script setup>
-import AnimProp from '../AnimProp.js'
-import getControlVector from '../ControlVector.js'
-import { cubicBezier, cubicBezierSlope } from '../CubicBezier.js'
-</script>
-
 <script>
-import BezierEasing from 'bezier-easing'
-// https://css-tricks.com/ease-out-in-ease-in-out/
-const easeOut = BezierEasing(0, 0, 0.58, 1)
 const colors = ['red', 'green', 'blue', 'indigo', 'pink']
 export default {
   data() {
     return {
       oldNode: this.node,
-      phase: new AnimProp(1, 500, easeOut, 1.5/500),
-      stickyAngle: 0,
     }
   },
   props: {
     id: Number,
     node: Number,
     nodeCount: Number,
-    loop: Array,
-    dLength: Number,
   },
   computed: {
     transformation() {
-      return `translate(${this.x} ${this.y}) rotate(${this.stickyAngle * 180 / Math.PI})`
+      return `translate(${this.x} ${this.y})`
     },
     shape() {
       let radius = 0.1
       let yRadius = radius * Math.sqrt(0.75)
       return `M 0 ${-yRadius} L ${radius} ${yRadius} H ${-radius} Z`
     },
-    angle() {
-      if (this.indexInLoop < 0) { return null }
-      return (this.counterclockwise ? Math.PI : 0) + Math.atan2(
-        this.xBezierSlope(this.phase.value),
-        -this.yBezierSlope(this.phase.value),
-      )
-    },
     x() {
-      if (this.xBezier === null) { return this.nodeX }
-      return this.xBezier(this.phase.value)
+      return Math.sin(2 * Math.PI * this.node / this.nodeCount)
     },
     y() {
-      if (this.yBezier === null) { return this.nodeY }
-      return this.yBezier(this.phase.value)
+      return -Math.cos(2 * Math.PI * this.node / this.nodeCount)
     },
     color() {
       return colors[this.id]
-    },
-    xBezier() {
-      if (this.indexInLoop < 0) { return null }
-      return cubicBezier(
-        this.otherX,
-        this.otherX + this.d1.x,
-        this.nodeX + this.d2.x,
-        this.nodeX,
-      )
-    },
-    yBezier() {
-      if (this.indexInLoop < 0) { return null }
-      return cubicBezier(
-        this.otherY,
-        this.otherY + this.d1.y,
-        this.nodeY + this.d2.y,
-        this.nodeY,
-      )
-    },
-    xBezierSlope() {
-      if (this.indexInLoop < 0) { return null }
-      return cubicBezierSlope(
-        this.otherX,
-        this.otherX + this.d1.x,
-        this.nodeX + this.d2.x,
-        this.nodeX,
-      )
-    },
-    yBezierSlope() {
-      if (this.indexInLoop < 0) { return null }
-      return cubicBezierSlope(
-        this.otherY,
-        this.otherY + this.d1.y,
-        this.nodeY + this.d2.y,
-        this.nodeY,
-      )
-    },
-    d1() {
-      if (this.indexInLoop < 0) { return null }
-      return getControlVector(
-        this.nodeX - this.otherX,
-        this.nodeY - this.otherY,
-        this.tailX - this.otherX,
-        this.tailY - this.otherY,
-        this.dLength,
-      )
-    },
-    d2() {
-      if (this.indexInLoop < 0) { return null }
-      return getControlVector(
-        this.otherX - this.nodeX,
-        this.otherY - this.nodeY,
-        this.headX - this.nodeX,
-        this.headY - this.nodeY,
-        this.dLength,
-      )
-    },
-    headX() { return this.getNodeX(this.head) },
-    headY() { return this.getNodeY(this.head) },
-    nodeX() { return this.getNodeX(this.node) },
-    nodeY() { return this.getNodeY(this.node) },
-    otherX() { return this.getNodeX(this.other) },
-    otherY() { return this.getNodeY(this.other) },
-    tailX() { return this.getNodeX(this.tail) },
-    tailY() { return this.getNodeY(this.tail) },
-    head() {
-      if (this.indexInLoop < 0) { return null }
-      return this.counterclockwise ? this.getNthNode(-1) : this.getNthNode(1)
-    },
-    other() {
-      if (this.indexInLoop < 0) { return null }
-      return this.counterclockwise ? this.oldNode : this.getNthNode(-1)
-    },
-    tail() {
-      if (this.indexInLoop < 0) { return null }
-      return this.counterclockwise ? this.getNthNode(2) : this.getNthNode(-2)
-    },
-    counterclockwise() {
-      if (this.indexInLoop < 0) { return null }
-      return this.oldNode == this.getNthNode(1)
-    },
-    indexInLoop() {
-      return this.loop.indexOf(this.node)
-    }
-  },
-  methods: {
-    getNthNode(offset) {
-      if (this.indexInLoop < 0) { return null }
-      return this.loop[(this.indexInLoop + this.loop.length + offset) % this.loop.length]
-    },
-    getNodeX(node) {
-      if (node === null) { return null }
-      return Math.sin(2 * Math.PI * node / this.nodeCount)
-    },
-    getNodeY(node) {
-      if (node === null) { return null }
-      return -Math.cos(2 * Math.PI * node / this.nodeCount)
-    },
-  },
-  watch: {
-    node(newNode, oldNode) {
-      let reverse = newNode == this.oldNode
-      this.oldNode = oldNode
-      if (reverse) {
-        this.phase.set(1 - this.phase.value, 0)
-      } else {
-        this.phase.set(0, 0)
-      }
-
-      this.phase.set(1)
-    },
-    loop(newLoop, oldLoop) {
-      this.oldNode = this.node
-      this.phase.set(1, 0)
-    },
-    angle: {
-      handler(newAngle, oldAngle) {
-        if (newAngle !== null) {
-          this.stickyAngle = newAngle
-        }
-      },
-      immediate: true,
     },
   },
 }
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -6,53 +6,55 @@ import Bead from './Bead.vue'
 <script>
 export default {
   data() {
+    let beadSet = new Set(this.startingBeads)
+    let hole = 0
+    for (; beadSet.has(hole); hole++) { }
+    let count = this.startingBeads.length + 1
+    let inLowerHalf = 4 * hole > count && 4 * hole <= 3 * count
+    let sign = inLowerHalf ? 1 : -1
+    let firstTail
+    let firstScore = count
+    for (let edge of this.edges) {
+      let tail
+      if (edge[0] == hole) {
+        tail = edge[1]
+      } else if (edge[1] == hole) {
+        tail = edge[0]
+      } else {
+        continue
+      }
+
+      let score = ((tail - hole) * sign + count) % count
+      if (score < firstScore) {
+        firstScore = score
+        firstTail = tail
+      }
+    }
+
     return {
       beads: [...this.startingBeads],
-      loopIndex: 0,
-      spin: false
+      history: [hole, firstTail]
     }
   },
   props: {
     startingBeads: Array,
-    edges: Array,
-    loops: Array
+    edges: Array
   },
   computed: {
     edgeData() {
-      let loop = this.loops[this.loopIndex]
+      let hole = this.history[this.history.length - 2]
+      let tail = this.history[this.history.length - 1]
       return this.edges.map(
         (edge, index, edges) => {
-          let offset = loop.indexOf(edge[0])
-          let prevBead = edge[0]
-          let nextBead = edge[1]
-          let active = false
-          if (offset >= 0) {
-            if (loop[(offset + 1) % loop.length] == edge[1]) {
-              prevBead = loop[(offset + loop.length - 1) % loop.length]
-              nextBead = loop[(offset + 2) % loop.length]
-              active = true
-            }
-            else if (loop[(offset + loop.length - 1) % loop.length] == edge[1]) {
-              prevBead = loop[(offset + 1) % loop.length]
-              nextBead = loop[(offset + loop.length - 2) % loop.length]
-              active = true
-            }
-          }
           return {
-            prev: this.getBeadPosition(prevBead),
+            prev: this.getBeadPosition(edge[0]),
             start: this.getBeadPosition(edge[0]),
             stop: this.getBeadPosition(edge[1]),
-            next: this.getBeadPosition(nextBead),
-            active: active,
+            next: this.getBeadPosition(edge[1]),
+            active: (edge[0] == hole && edge[1] == tail) || (edge[0] == tail && edge[1] == hole),
           }
         }
       )
-    },
-    hole() {
-      let beadSet = new Set(this.beads)
-      let hole = 0
-      for (; hole <= beadSet.size && beadSet.has(hole); hole++) { }
-      return hole
     },
   },
   methods: {
@@ -62,68 +64,140 @@ export default {
         y: -Math.cos(2 * Math.PI * index / (1 + this.beads.length))
       }
     },
-    prevLoop() {
-      do {
-        this.loopIndex += this.loops.length - 1
-        this.loopIndex %= this.loops.length
-      } while (!this.loops[this.loopIndex].includes(this.hole))
-    },
-    nextLoop() {
-      do {
-        this.loopIndex++
-        this.loopIndex %= this.loops.length
-      } while (!this.loops[this.loopIndex].includes(this.hole))
-    },
-    counterclockwise() {
-      let loop = this.loops[this.loopIndex]
-      // important to cache this property so it doesn't change during the for loop
-      let hole = this.hole
-      if (this.spin) {
-        for (let i = 0; i < this.beads.length; i++) {
-          let loopIndex = loop.indexOf(this.beads[i])
-          if (loopIndex >= 0) {
-            this.beads[i] = loop[(loopIndex + loop.length - 1) % loop.length]
-            if (this.beads[i] == hole) {
-              this.beads[i] = loop[(loopIndex + loop.length - 2) % loop.length]
-            }
-          }
+    goForward() {
+      let hole = this.history[this.history.length - 2]
+      let tail = this.history[this.history.length - 1]
+      this.beads[this.beads.indexOf(tail)] = hole
+      let oldTail = hole
+      hole = tail
+      for (let removeCount = this.history.length - 2; removeCount >= 0; removeCount--) {
+        if (this.history[removeCount] == hole) {
+          this.history = this.history.slice(removeCount)
+          this.history.push(this.history[1])
+          return
         }
-      } else {
-        let src = loop[(loop.indexOf(hole) + 1) % loop.length]
-        this.beads[this.beads.indexOf(src)] = hole
+      }
+
+      let possibleTails = new Set()
+      for (let edge of this.edges) {
+        if (edge[0] == hole) {
+          possibleTails.add(edge[1])
+        } else if (edge[1] == hole) {
+          possibleTails.add(edge[0])
+        }
+      }
+
+      possibleTails.delete(oldTail)
+
+      for (let i = this.history.length - 3; i >= 0; i--) {
+        if (possibleTails.has(this.history[i])) {
+          this.history.push(this.history[i])
+          return
+        }
+      }
+
+      let count = this.beads.length + 1
+      let inLowerHalf = 4 * hole > count && 4 * hole <= 3 * count
+      let sign = inLowerHalf ? 1 : -1
+      let firstTail = oldTail
+      let firstScore = count
+      for (let tail of possibleTails) {
+        let score = ((tail - hole) * sign + count) % count
+        if (score < firstScore) {
+          firstScore = score
+          firstTail = tail
+        }
+      }
+
+      this.history.push(firstTail)
+    },
+    goBack() {
+      if (this.history.length > 2) {
+        this.history.pop()
+        let hole = this.history[this.history.length - 2]
+        let tail = this.history[this.history.length - 1]
+        this.beads[this.beads.indexOf(hole)] = tail
+        if (this.history[0] == tail) {
+          // ensure the entire loop is represented
+          this.history.unshift(hole)
+        }
       }
     },
-    clockwise() {
-      let loop = this.loops[this.loopIndex]
-      // important to cache this property so it doesn't change during the for loop
-      let hole = this.hole
-      if (this.spin) {
-        for (let i = 0; i < this.beads.length; i++) {
-          let loopIndex = loop.indexOf(this.beads[i])
-          if (loopIndex >= 0) {
-            this.beads[i] = loop[(loopIndex + 1) % loop.length]
-            if (this.beads[i] == hole) {
-              this.beads[i] = loop[(loopIndex + 2) % loop.length]
-            }
-          }
+    selectLeft() {
+      let hole = this.history[this.history.length - 2]
+      let count = this.beads.length + 1
+      let inLowerHalf = 4 * hole > count && 4 * hole <= 3 * count
+      let sign = inLowerHalf ? 1 : -1
+      let oldTail = this.history[this.history.length - 1]
+      let oldScore = ((oldTail - hole) * sign + count) % count
+      let newTail = oldTail
+      let newScore = 0
+      let maxTail
+      let maxScore = 0
+      for (let edge of this.edges) {
+        let tail
+        if (edge[0] == hole) {
+          tail = edge[1]
+        } else if (edge[1] == hole) {
+          tail = edge[0]
+        } else {
+          continue
         }
-      } else {
-        let src = loop[(loop.indexOf(hole) + loop.length - 1) % loop.length]
-        this.beads[this.beads.indexOf(src)] = hole
+
+        let score = ((tail - hole) * sign + count) % count
+        if (score < oldScore && score > newScore) {
+          newScore = score
+          newTail = tail
+        } else if (score > maxScore) {
+          maxScore = score
+          maxTail = tail
+        }
       }
+
+      this.history[this.history.length - 1] = newTail == oldTail ? maxTail : newTail
     },
-    toggleSpin() {
-      this.spin = !this.spin
+    selectRight() {
+      let hole = this.history[this.history.length - 2]
+      let count = this.beads.length + 1
+      let inLowerHalf = 4 * hole > count && 4 * hole <= 3 * count
+      let sign = inLowerHalf ? 1 : -1
+      let oldTail = this.history[this.history.length - 1]
+      let oldScore = ((oldTail - hole) * sign + count) % count
+      let newTail = oldTail
+      let newScore = count
+      let minTail
+      let minScore = count
+      for (let edge of this.edges) {
+        let tail
+        if (edge[0] == hole) {
+          tail = edge[1]
+        } else if (edge[1] == hole) {
+          tail = edge[0]
+        } else {
+          continue
+        }
+
+        let score = ((tail - hole) * sign + count) % count
+        if (score > oldScore && score < newScore) {
+          newScore = score
+          newTail = tail
+        } else if (score < minScore) {
+          minScore = score
+          minTail = tail
+        }
+      }
+
+      this.history[this.history.length - 1] = newTail == oldTail ? minTail : newTail
     }
   }
 }
 </script>
 
 <template>
-  <button class="tabStop" @keydown.up.stop.prevent="prevLoop()" @keydown.down.stop.prevent="nextLoop()" @keydown.left.stop.prevent="counterclockwise()" @keydown.right.stop.prevent="clockwise()" @keydown.0.stop.prevent="toggleSpin()">
+  <button class="tabStop" @keydown.up.stop.prevent="goForward()" @keydown.down.stop.prevent="goBack()" @keydown.left.stop.prevent="selectLeft()" @keydown.right.stop.prevent="selectRight()">
     <svg class="gameView" viewBox="-1.1 -1.1 2.2 2.2">
       <Edge v-for="edge of edgeData" :dLength="0.3" v-bind:x0="edge.prev.x" v-bind:y0="edge.prev.y" v-bind:x1="edge.start.x" v-bind:y1="edge.start.y" v-bind:x2="edge.stop.x" v-bind:y2="edge.stop.y" v-bind:x3="edge.next.x" v-bind:y3="edge.next.y" v-bind:active="edge.active" />
-      <Bead v-for="(node, id) of beads" :dLength="0.3" v-bind:id="id" v-bind:node="node" :nodeCount="beads.length + 1" :loop="this.loops[this.loopIndex]" />
+      <Bead v-for="(node, id) of beads" :dLength="0.3" v-bind:id="id" v-bind:node="node" :nodeCount="beads.length + 1" />
     </svg>
   </button>
 </template>
