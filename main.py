0c002fc51584aad14b59668228a27ecc8307425b
Eva Short <evashort73@gmail.com>
2022-08-08 10:53:40 -0700

    check in all 11 5-node puzzles

--- main.py
+++ main.py
@@ -374,7 +374,7 @@ def get_stem(graph, labels='abcdefghijklmnopqrstuvwxyz'):
 if __name__ == '__main__':
     folder = Path('graphs')
     folder.mkdir(exist_ok=True)
-    nodes = 7
+    nodes = 5
     distances = get_empty_distances(nodes)
     out = np.empty_like(distances)
     temp = np.empty_like(distances)
