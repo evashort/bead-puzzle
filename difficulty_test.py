0bfaf16ef025c4425c3b95309b92d500f8cd2f6e
Eva Short <evashort73@gmail.com>
2022-08-10 20:19:48 -0700

    combine graphs into one file sorted by difficulty

--- difficulty_test.py
+++ difficulty_test.py
@@ -5,12 +5,17 @@ import random
 
 iterations = 1000
 src_folder = Path('graphs')
-dst_folder = Path('src/assets/graphs')
+dst_folder = Path('difficulty')
 dst_folder.mkdir(exist_ok=True)
+final_path = Path('src/assets/graphs.json')
+graphs = []
 for i, src_path in enumerate(src_folder.iterdir()):
     print(f'{i + 1} {src_path.stem}', flush=True)
+    with open(src_path, encoding='utf-8') as f:
+        graph = json.load(f)
+
     dst_path = dst_folder / src_path.name
-    result = None
+    difficulty = None
     try:
         f = open(dst_path, encoding='utf-8')
     except FileNotFoundError:
@@ -18,24 +23,21 @@ for i, src_path in enumerate(src_folder.iterdir()):
     else:
         with f:
             try:
-                result = json.load(f)
+                difficulty = json.load(f)
             except json.decoder.JSONDecodeError:
                 pass
 
-    if result is None:
-        with open(src_path, encoding='utf-8') as f:
-            result = json.load(f)
-
-        node_count = result['nodes']
+    if difficulty is None:
+        node_count = graph['nodes']
         goal = tuple(range(1, node_count))
         total_difficulty = 0
         for i in range(iterations):
-            puzzle = random.choice(result['puzzles'])
+            puzzle = random.choice(graph['puzzles'])
             beads = tuple(puzzle['beads'])
             hole, = set(goal + (0,)).difference(beads)
             edges = {}
             rotation = puzzle['rotation']
-            for a, b in result['edges']:
+            for a, b in graph['edges']:
                 a, b = ((x + rotation) % node_count for x in (a, b))
                 edges.setdefault(a, set()).add(b)
                 edges.setdefault(b, set()).add(a)
@@ -62,6 +64,17 @@ for i, src_path in enumerate(src_folder.iterdir()):
 
             total_difficulty += moves
         
-        result['difficulty'] = total_difficulty / iterations
+        difficulty = total_difficulty / iterations
         with open(dst_path, mode='w', encoding='utf-8') as f:
-            json.dump(result, f)
+            json.dump(difficulty, f)
+
+        # reduce precision before sorting
+        with open(dst_path, encoding='utf-8') as f:
+            difficulty = json.load(f)
+
+    graph['difficulty'] = difficulty
+    graphs.append(graph)
+
+graphs.sort(key=lambda graph: graph['difficulty'])
+with open(final_path, mode='w', encoding='utf-8') as f:
+    json.dump({'graphs': graphs}, f)
