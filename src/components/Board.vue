965598cb39d77ae877819b64f324ff2996eb8ef2
Eva Short <evashort73@gmail.com>
2023-11-26 04:26:46 -0800

    split game into components WIP 1

---
+++ src/components/Board.vue
@@ -0,0 +1,93 @@
+<script setup>
+import Edge from './Edge.vue'
+import SimpleGraph from '../SimpleGraph.js'
+import Permute from '../Permute.js'
+</script>
+
+<script>
+export default {
+  data() {
+    let size = SimpleGraph.bytesToNodeCount(
+      SimpleGraph.fromString(this.graphId)
+    )
+    let beadStarts = []
+    for (let bead = 1; bead < size; bead++) {
+      beadStarts.push(Permute.findValue(this.beads, bead))
+    }
+    return {
+      beadStarts: beadStarts,
+    }
+  },
+  props: {
+    graphId: String,
+    beads: Number,
+    history: Array,
+  },
+  computed: {
+    graph() {
+      return SimpleGraph.fromString(this.graphId)
+    },
+    size() {
+      return SimpleGraph.bytesToNodeCount(this.graph)
+    },
+    edges() {
+      let result = {}
+      for (let b = 0; b < this.size; b++) {
+        for (let a = 0; a < b; a++) {
+          if (SimpleGraph.hasEdge(this.graph, a, b)) {
+            result[[a, b].toString()] = [a, b]
+          }
+        }
+      }
+
+      return result
+    },
+    edgeBeads() {
+      let result = {}
+      for (let bead = 1; bead < this.size; bead++) {
+        let a = this.beadStarts[bead - 1]
+        let b = Permute.findValue(this.beads, bead)
+        if (b < a) {
+          [a, b] = [b, a]
+        }
+
+        result[[a, b].toString()] = bead
+      }
+    }
+  },
+  watch: {
+    beads(newBeads, oldBeads) {
+      let newBeadStarts = []
+      for (let bead = 1; bead < this.size; bead++) {
+        let start = Permute.findValue(oldBeads, bead)
+        let end = Permute.findValue(newBeads, bead)
+        if (end != start) {
+          newBeadStarts.push(start)
+        } else {
+          newBeadStarts.push(this.beadStarts[bead - 1])
+        }
+      }
+
+      this.beadStarts = newBeadStarts
+    }
+  }
+}
+</script>
+
+<template>
+  <Edge
+    v-for="([a, b], key) in this.edges"
+    :key="key"
+    :size="size"
+    :a="a"
+    :b="b"
+    :aPrime="a"
+    :bPrime="b"
+    :facingA="false"
+    :moveToA="false"
+    :onPath="false"
+    :moving="false"
+    :id="edgeBeads[key] ?? 0"
+    :selected="false"
+  />
+</template>
---
+++ src/components/Edge.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+  props: {
+    size: Number,
+    a: Number,
+    b: Number,
+    aPrime: Number,
+    bPrime: Number,
+    facingA: Boolean,
+    moveToA: Boolean,
+    onPath: Boolean,
+    moving: Boolean,
+    id: Number,
+    selected: Boolean,
+  },
+  computed: {
+    name() {
+      if (this.id > 0) {
+        return [
+          [],
+          [],
+          ['mushroom'],
+          ['butterfly', 'mushroom'],
+          ['heart', 'butterfly', 'mushroom'],
+          ['heart', 'butterfly', 'leaf', 'mushroom'],
+          ['heart', 'butterfly', 'leaf', 'mushroom', 'flower'],
+          ['heart', 'butterfly', 'mushroom', 'leaf', 'mushroom', 'flower'],
+        ][this.size][this.id]
+      }
+
+      return null
+    },
+    scale() {
+      if (this.name) {
+        return {
+          heart: 2.7,
+          butterfly: 2.8,
+          saturn: 3.35,
+          leaf: 2.5,
+          mushroom: 2.6,
+          flower: 2.5,
+        }[this.name] * (this.selected ? 5/3 : 1)
+      }
+
+      return 1
+    },
+    beadClasses() {
+      return {
+        facingA: facingA,
+        moveToA: moveToA,
+        onPath: onPath,
+        moving: moving,
+      }
+    },
+  },
+}
+</script>
+
+<template>
+  <use
+    :href="`#${name}-bead`"
+    :key="name"
+    :class="beadClasses"
+    :style="{ '--scale': scale }"
+  />
+</template>
+
+<style scoped>
+/*
+tail onPath undo reverse offset-rotate
+0    0                   -90deg
+0    1           0       auto
+0    1           1       reverse
+1                        reverse
+*/
+.bead {
+  offset-distance: 100%;
+  offset-rotate: auto;
+  transform: rotate(90deg) scale(var(--scale));
+}
+.bead.tail {
+  offset-rotate: reverse;
+}
+.canAnimate .bead.animate {
+  animation: slide 0.75s ease forwards;
+}
+@keyframes slide {
+  from { offset-distance: 0%; }
+  to { offset-distance: 100%; }
+}
+.canAnimate .bead.alternate {
+  animation-name: slide2;
+}
+@keyframes slide2 {
+  from { offset-distance: 0%; }
+  to { offset-distance: 100%; }
+}
+</style>
